d4341a882da4b4a40e6db6bceba433a305f1e708
Steve Konves <stephen.konves@gmail.com>
2018-07-28 11:56:15 -0700

    fix: accept Patch<T>

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export type Primative = Symbol | string | number | boolean;
 export interface Definition<T> {
   getPayload(payload: T): T;
-  getPatch(payload: T): Patch<T>;
+  getPatch(payload: Patch<T>): Patch<T>;
   getKey(payload: T): string;
 }
 export type Index<T> = { [key: string]: T };
