bdc8035bc1622f1e51d59747ebe2c8b3b3c2ed45
Steve Konves <skonves@bolste.com>
2018-08-04 10:39:33 -0700

    test: add test cases for complex children

--- src/functions/set.tests.ts
+++ src/functions/set.tests.ts
@@ -1,15 +1,47 @@
 import { expect } from 'chai';
 
-import { Definition, set, Index } from '..';
+import { Definition, set, Index, define } from '..';
+import {
+  key as keyRule,
+  required,
+  optional,
+  objectOf,
+  indexOf,
+  array,
+} from '../rules';
 
-type TestItem = {
+type TestChildItem = {
   id: string;
   name: string;
   value?: number;
 };
 
+const childDef = define<TestChildItem>({
+  id: keyRule(),
+  name: required(),
+  value: optional(),
+});
+
+type TestItem = {
+  id: string;
+  name: string;
+  value?: number;
+  child?: TestChildItem;
+  children?: Index<TestChildItem>;
+  items?: number[];
+};
+
+const parentDef = define<TestItem>({
+  id: keyRule(),
+  name: required(),
+  value: optional(),
+  child: optional(objectOf(childDef)),
+  children: optional(indexOf(childDef)),
+  items: optional(array()),
+});
+
 describe('set', () => {
-  describe('primitive array', () => {
+  describe('target primitive array', () => {
     it('Adds a new value', () => {
       // ARRANGE
       const target = [1, 2, 3];
@@ -22,7 +54,7 @@ describe('set', () => {
       expect(result).to.have.members([1, 2, 3, 4]);
     });
 
-    it('No-ops when value alread exists', () => {
+    it('No-ops when value already exists', () => {
       // ARRANGE
       const target = [1, 2, 3];
       const payload = 3;
@@ -36,87 +68,299 @@ describe('set', () => {
     });
   });
 
-  describe('object', () => {
-    it('Adds a new value', () => {
-      // ARRANGE
-      const target: TestItem = {
-        id: 'QWERTY',
-        name: 'asdf',
-      };
-      const key = 'value';
-      const payload = 7;
-      const definition: Definition<TestItem> = {
-        getPayload: x => null,
-        getPatch: x => ({ value: payload }),
-        getKey: x => x.id,
-        getDefinitions: key => null,
-      };
+  describe('target object', () => {
+    describe('with a primitive property value', () => {
+      it('Adds a new primitive value', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'value';
+        const payload = 7;
+        // const definition: Definition<TestItem> = {
+        //   getPayload: x => null,
+        //   getPatch: x => ({ [key]: payload }),
+        //   getKey: x => x.id,
+        //   getDefinitions: key => null,
+        // };
 
-      const expected: TestItem = {
-        id: 'QWERTY',
-        name: 'asdf',
-        value: 7,
-      };
+        const definition = parentDef;
 
-      // ACT
-      const result = set(target, key, payload, definition);
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          value: 7,
+        };
 
-      // ASSERT
-      expect(result).to.deep.equal(expected);
+        // ACT
+        const result = set(target, key, payload, definition);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('Overwrites an existing primitive value', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          value: 7,
+        };
+        const key = 'value';
+        const payload = 18;
+
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          value: 18,
+        };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('No-ops when setting an immutable value', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'id';
+        const payload = 'THE NEW ID';
+
+        const expected: TestItem = { ...target };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+        expect(result).to.equal(target);
+      });
+
+      it('No-ops when property value is not updated', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          value: 7,
+        };
+        const key = 'value';
+        const payload = 7;
+
+        const expected: TestItem = { ...target };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+        expect(result).to.equal(target);
+      });
     });
 
-    it('Overwrites an existing value', () => {
-      // ARRANGE
-      const target: TestItem = {
-        id: 'QWERTY',
-        name: 'asdf',
-        value: 7,
-      };
-      const key = 'value';
-      const payload = 18;
-      const definition: Definition<TestItem> = {
-        getPayload: x => null,
-        getPatch: x => ({ value: payload }),
-        getKey: x => x.id,
-        getDefinitions: key => null,
-      };
+    describe('with an object property value', () => {
+      it('Adds a new object value', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'child';
+        const payload = { id: 'child id', name: 'child name' };
 
-      const expected: TestItem = {
-        id: 'QWERTY',
-        name: 'asdf',
-        value: 18,
-      };
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          child: { id: 'child id', name: 'child name' },
+        };
 
-      // ACT
-      const result = set(target, key, payload, definition);
+        // ACT
+        const result = set(target, key, payload, parentDef);
 
-      // ASSERT
-      expect(result).to.deep.equal(expected);
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('Overwrites an existing object', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          child: { id: 'child id', name: 'child name', value: 7 },
+        };
+        const key = 'child';
+        const payload = { id: 'new child id', name: 'new child name' };
+
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          child: { id: 'new child id', name: 'new child name' },
+        };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('No-ops when new object is invalid', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'child';
+        const payload = { id: 'child id' };
+
+        const expected: TestItem = { ...target };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+        expect(result).to.equal(target);
+      });
     });
 
-    it('No-ops when patch is falsy', () => {
-      // ARRANGE
-      const target: TestItem = {
-        id: 'QWERTY',
-        name: 'asdf',
-      };
-      const key = 'value';
-      const payload = 7;
-      const definition: Definition<TestItem> = {
-        getPayload: x => null,
-        getPatch: x => null,
-        getKey: x => x.id,
-        getDefinitions: key => null,
-      };
+    describe('with an index property value', () => {
+      it('Adds a new index value', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'children';
+        const payload = { a: { id: 'a', name: 'child name' } };
 
-      const expected: TestItem = { ...target };
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          children: { a: { id: 'a', name: 'child name' } },
+        };
 
-      // ACT
-      const result = set(target, key, payload, definition);
+        // ACT
+        const result = set(target, key, payload, parentDef);
 
-      // ASSERT
-      expect(result).to.deep.equal(expected);
-      expect(result).to.equal(target);
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('Overwrites an existing index', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          children: { a: { id: 'a', name: 'child name' } },
+        };
+        const key = 'children';
+        const payload = { b: { id: 'b', name: 'new child name' } };
+
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          children: { b: { id: 'b', name: 'new child name' } },
+        };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('No-ops when new index is invalid', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          children: { a: { id: 'a', name: 'child name' } },
+        };
+        const key = 'children';
+        const payload = { b: { id: 'b' } };
+
+        const expected: TestItem = { ...target };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+        expect(result).to.equal(target);
+      });
+    });
+
+    describe('with a primitive array property value', () => {
+      it('Adds a new primitive array', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+        };
+        const key = 'items';
+        const payload = [1, 2, 3];
+
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          items: [1, 2, 3],
+        };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('Overwrites an existing primitive array', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          items: [1, 2, 3],
+        };
+        const key = 'items';
+        const payload = [3, 4, 5];
+
+        const expected: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          items: [3, 4, 5],
+        };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+      });
+
+      it('No-ops when new array is falsy', () => {
+        // ARRANGE
+        const target: TestItem = {
+          id: 'QWERTY',
+          name: 'asdf',
+          items: [1, 2, 3],
+        };
+        const key = 'items';
+        const payload = null;
+
+        const expected: TestItem = { ...target };
+
+        // ACT
+        const result = set(target, key, payload, parentDef);
+
+        // ASSERT
+        expect(result).to.deep.equal(expected);
+        expect(result).to.equal(target);
+      });
     });
   });
 
